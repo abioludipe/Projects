c8fac3f7f2fe25b8061a3635b3681e7c80fa34a7
Abimbola Oludipe <ao.oludipe@gmail.com>
2022-10-29 15:02:46 +0100

    Initials project

---
+++ initials/initials.py
@@ -0,0 +1,11 @@
+# My name is Abimbola Oludipe
+""" Fun Fact about me is:
+I play the lead guitar"""
+
+print('  A    OOO')
+print(' A A  O   O')
+print('A   A O   O')
+print('AAAAA O   O')
+print('A   A O   O')
+print('A   A O   O')
+print('A   A  OOO ')
